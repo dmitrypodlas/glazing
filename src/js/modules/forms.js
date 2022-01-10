7345b45a7f8f79f8a42945070a8e1449ac809799
Dmitrij <dvpodlas@gmail.com>
2022-01-10 20:57:19 +0200

    working with calculator form

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,18 +1,15 @@
-const forms = () => {
+import checkNumInputs from './checkNumInputs';
+
+const forms = (state) => {
     const form = document.querySelectorAll('form'),
-          inputs = document.querySelectorAll('input'),
-          phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+          inputs = document.querySelectorAll('input');
 
-    phoneInputs.forEach(item => {
-        item.addEventListener('input', () => {
-            item.value = item.value.replace(/\D/, '');
-        });
-    });
-
+    checkNumInputs('input[name="user_phone"]');
+    
     const message = {
         loading: 'Загрузка...',
         success: 'Спасибо! Скоро мы с вами свяжемся',
-        failure: 'Упс...Что-то пошло не так'
+        failure: 'Что-то пошло не так...'
     };
 
     const postData = async (url, data) => {
@@ -40,6 +37,11 @@ const forms = () => {
             item.appendChild(statusMessage);
 
             const formData = new FormData(item);
+            if (item.getAttribute('data-calc') === "end") {
+                for (let key in state) {
+                    formData.append(key, state[key]);
+                }
+            }
 
             postData('assets/server.php', formData)
                 .then(res => {
